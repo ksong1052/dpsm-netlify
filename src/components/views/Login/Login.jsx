b5ab2eb976eed63d914ce47e99b6e53dfa082fb8
dnaielsong <danielsong1052@gmail.com>
2022-06-23 10:03:33 -0300

    fixed Login and Register component

--- src/components/views/Login/Login.jsx
+++ src/components/views/Login/Login.jsx
@@ -1,153 +1,85 @@
-import React, { useState } from "react";
-import { loginUser } from "../../../_actions/user_action";
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import { Form, Input, Button, Checkbox, Typography } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import './login.scss';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { loginUser } from "../../../_actions/user_action";
 
-const { Title } = Typography;
-
-function Login(props) {
+const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const rememberMeChecked = localStorage.getItem("rememberMe") ? true : false;
 
-  const [formErrorMessage, setFormErrorMessage] = useState('')
-  const [rememberMe, setRememberMe] = useState(rememberMeChecked)
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
 
-  const handleRememberMe = () => {
-    setRememberMe(!rememberMe)
-  };
+  const emaiHandler =(e) => {
+    setEmail(e.currentTarget.value);
+  }
 
-  const initialEmail = localStorage.getItem("rememberMe") ? localStorage.getItem("rememberMe") : '';
+  const passwordHandler =(e) => {
+    setPassword(e.currentTarget.value);
+  }
+
+  const onSubmitLogin = (e) => {
+    e.preventDefault();
+
+    let dataToSubmit = {
+      email: Email,
+      password: Password
+    }
+
+    dispatch(loginUser(dataToSubmit)).then(response => {
+      if (response.payload.loginSuccess) {
+        navigate("/");
+      } else {
+        alert('Failed to login')
+      }
+    })         
+  }
 
   return (
-    <Formik
-      initialValues={{
-        email: initialEmail,
-        password: '',
-      }}
-      validationSchema={Yup.object().shape({
-        email: Yup.string()
-          .email('Email is invalid')
-          .required('Email is required'),
-        password: Yup.string()
-          .min(6, 'Password must be at least 6 characters')
-          .required('Password is required'),
-      })}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          let dataToSubmit = {
-            email: values.email,
-            password: values.password
-          };
-
-          dispatch(loginUser(dataToSubmit))
-            .then(response => {
-              if (response.payload.loginSuccess) {
-                window.localStorage.setItem('userId', response.payload.userId);
-                if (rememberMe === true) {
-                  window.localStorage.setItem('rememberMe', values.id);
-                } else {
-                  localStorage.removeItem('rememberMe');
-                }
-                navigate("/");
-              } else {
-                setFormErrorMessage('Check out your Account or Password again')
-              }
-            })
-            .catch(err => {
-              setFormErrorMessage('Check out your Account or Password again')
-              setTimeout(() => {
-                setFormErrorMessage("")
-              }, 3000);
-            });
-          setSubmitting(false);
-        }, 500);
-      }}
-    >
-      {props => {
-        const {
-          values,
-          touched,
-          errors,
-          dirty,
-          isSubmitting,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          handleReset,
-        } = props;
-        return (
-          <div className="app">
-
-            <Title level={2}>Log In</Title>
-            <form onSubmit={handleSubmit} style={{ width: '350px' }}>
-
-              <Form.Item required>
-                <Input
-                  id="email"
-                  // prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  prefix={<UserOutlined />} 
-                  placeholder="Enter your email"
-                  type="email"
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={
-                    errors.email && touched.email ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.email && touched.email && (
-                  <div className="input-feedback">{errors.email}</div>
-                )}
-              </Form.Item>
-
-              <Form.Item required>
-                <Input
-                  id="password"
-                  // prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  prefix={<LockOutlined />} 
-                  placeholder="Enter your password"
-                  type="password"
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={
-                    errors.password && touched.password ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.password && touched.password && (
-                  <div className="input-feedback">{errors.password}</div>
-                )}
-              </Form.Item>
-
-              {formErrorMessage && (
-                <label ><p style={{ color: '#ff0000bf', fontSize: '0.7rem', border: '1px solid', padding: '1rem', borderRadius: '10px' }}>{formErrorMessage}</p></label>
-              )}
-
-              <Form.Item>
-                {/* <Checkbox id="rememberMe" onChange={handleRememberMe} checked={rememberMe} >Remember me</Checkbox>
-                  <a className="login-form-forgot" href="/reset_user" style={{ float: 'right' }}>
-                    forgot password
-                  </a> */}
-                <div>
-                  <Button type="primary" htmlType="submit" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
-                    Log in
-                  </Button>
-                </div>
-                Or <a href="/register">register now!</a>
-              </Form.Item>
-            </form>
+    <div className="login">
+      <div className="container">
+        <Form className="loginForm" onSubmit={onSubmitLogin}>
+          <div className="loginTitle">
+            <h2>Sign In</h2>
           </div>
-        );
-      }}
-    </Formik>
-  );
-};
 
-export default Login;
+          <Form.Group className="mb-3 lField" controlId="formBasicEmail">
+            <Form.Label className="lLabel">Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={Email}
+              onChange={emaiHandler}
+              className="formInput"
+            />
+          </Form.Group>
 
+          <Form.Group className="mb-3 lField" controlId="formBasicPassword">
+            <Form.Label className="lLabel">Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter Password"
+              value={Password}
+              onChange={passwordHandler}
+              className="formInput"
+            />
+          </Form.Group>
 
+          <div className="lButton">
+            <Button variant="primary" type="submit" className="formButton">
+              Login
+            </Button>
+          </div>
+          <div style={{ float: 'left' }}>
+            Don't you have an account? <Link to="/register" className="link">Sign Up Now!</Link> 
+          </div>
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- src/components/views/Register/Register.jsx
+++ src/components/views/Register/Register.jsx
@@ -1,206 +1,119 @@
-import React from "react";
-import moment from "moment";
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import { registerUser } from "../../../_actions/user_action";
+import { useState } from 'react';
+import './register.scss';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { registerUser } from "../../../_actions/user_action";
 
-import {
-  Form,
-  Input,
-  Button,
-} from 'antd';
-
-const formItemLayout = {
-  labelCol: {
-    xs: { span: 24 },
-    sm: { span: 8 },
-  },
-  wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 16 },
-  },
-};
-const tailFormItemLayout = {
-  wrapperCol: {
-    xs: {
-      span: 24,
-      offset: 0,
-    },
-    sm: {
-      span: 16,
-      offset: 8,
-    },
-  },
-};
-
-function Register(props) {
+const Register = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const [Name, setName] = useState("");
+  const [Lastname, setLastname] = useState("");  
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const nameHandler =(e) => {
+    setName(e.currentTarget.value);
+  }
+
+  const lastnameHandler =(e) => {
+    setLastname(e.currentTarget.value);
+  }
+
+  const emaiHandler =(e) => {
+    setEmail(e.currentTarget.value);
+  }
+
+  const passwordHandler =(e) => {
+    setPassword(e.currentTarget.value);
+  }
+
+  const onSubmitRegister = (e) => {
+    e.preventDefault();
+
+    let dataToSubmit = {
+      name: Name,
+      lastname: Lastname,
+      email: Email,
+      password: Password
+    }
+
+    dispatch(registerUser(dataToSubmit)).then(response => {
+      if (response.payload.registerSuccess) {
+        navigate("/login");
+      } else {
+        alert('Failed to register')
+      }
+    })    
+  }
+
   return (
-
-    <Formik
-      initialValues={{
-        email: '',
-        lastName: '',
-        name: '',
-        password: '',
-        confirmPassword: ''
-      }}
-      validationSchema={Yup.object().shape({
-        name: Yup.string()
-          .required('Name is required'),
-        lastName: Yup.string()
-          .required('Last Name is required'),
-        email: Yup.string()
-          .email('Email is invalid')
-          .required('Email is required'),
-        password: Yup.string()
-          .min(6, 'Password must be at least 6 characters')
-          .required('Password is required'),
-        confirmPassword: Yup.string()
-          .oneOf([Yup.ref('password'), null], 'Passwords must match')
-          .required('Confirm Password is required')
-      })}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-
-          let dataToSubmit = {
-            email: values.email,
-            password: values.password,
-            name: values.name,
-            lastname: values.lastname,
-            image: `http://gravatar.com/avatar/${moment().unix()}?d=identicon`
-          };
-
-          dispatch(registerUser(dataToSubmit)).then(response => {
-            if (response.payload.registerSuccess) {
-              navigate("/login");
-            } else {
-              alert('Failed to register')
-            }
-          })
-
-          setSubmitting(false);
-        }, 500);
-      }}
-    >
-      {props => {
-        const {
-          values,
-          touched,
-          errors,
-          dirty,
-          isSubmitting,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          handleReset,
-        } = props;
-        return (
-          <div className="app">
-            <h2>Sign up</h2>
-            <Form style={{ minWidth: '375px' }} {...formItemLayout} onSubmit={handleSubmit} >
-
-              <Form.Item required label="Name">
-                <Input
-                  id="name"
-                  placeholder="Enter your name"
-                  type="text"
-                  value={values.name}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={
-                    errors.name && touched.name ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.name && touched.name && (
-                  <div className="input-feedback">{errors.name}</div>
-                )}
-              </Form.Item>
-
-              <Form.Item required label="Last Name">
-                <Input
-                  id="lastName"
-                  placeholder="Enter your Last Name"
-                  type="text"
-                  value={values.lastName}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={
-                    errors.lastName && touched.lastName ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.lastName && touched.lastName && (
-                  <div className="input-feedback">{errors.lastName}</div>
-                )}
-              </Form.Item>
-
-              <Form.Item required label="Email" hasFeedback validateStatus={errors.email && touched.email ? "error" : 'success'}>
-                <Input
-                  id="email"
-                  placeholder="Enter your Email"
-                  type="email"
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={
-                    errors.email && touched.email ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.email && touched.email && (
-                  <div className="input-feedback">{errors.email}</div>
-                )}
-              </Form.Item>
-
-              <Form.Item required label="Password" hasFeedback validateStatus={errors.password && touched.password ? "error" : 'success'}>
-                <Input
-                  id="password"
-                  placeholder="Enter your password"
-                  type="password"
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={
-                    errors.password && touched.password ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.password && touched.password && (
-                  <div className="input-feedback">{errors.password}</div>
-                )}
-              </Form.Item>
-
-              <Form.Item required label="Confirm" hasFeedback>
-                <Input
-                  id="confirmPassword"
-                  placeholder="Enter your confirmPassword"
-                  type="password"
-                  value={values.confirmPassword}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={
-                    errors.confirmPassword && touched.confirmPassword ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.confirmPassword && touched.confirmPassword && (
-                  <div className="input-feedback">{errors.confirmPassword}</div>
-                )}
-              </Form.Item>
-
-              <Form.Item {...tailFormItemLayout}>
-                <Button onClick={handleSubmit} type="primary" disabled={isSubmitting}>
-                  Submit
-                </Button>
-              </Form.Item>
-            </Form>
+    <div className="register">
+      <div className="container">
+        <Form className="registerForm" onSubmit={onSubmitRegister}>
+          <div className="registerTitle">
+            <h2>Sign Up</h2>
           </div>
-        );
-      }}
-    </Formik>
-  );
-};
 
+          <Form.Group className="mb-3 rField" controlId="formBasicName">
+            <Form.Label className="rLabel">Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Name"
+              value={Name}
+              onChange={nameHandler}
+              className="formInput"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3 rField" controlId="formBasicLastname">
+            <Form.Label className="rLabel">Lastname</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter lastname"
+              value={Lastname}
+              onChange={lastnameHandler}
+              className="formInput"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3 rField" controlId="formBasicEmail">
+            <Form.Label className="rLabel">Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={Email}
+              onChange={emaiHandler}
+              className="formInput"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3 rField" controlId="formBasicPassword">
+            <Form.Label className="rLabel">Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter Password"
+              value={Password}
+              onChange={passwordHandler}
+              className="formInput"
+            />
+          </Form.Group>
+
+          <div className="rButton">
+            <Button variant="primary" type="submit" className="formButton">
+              Register
+            </Button>
+          </div>
+          <div style={{ float: 'left' }}>
+            Already have an account? <Link to="/login" className="link">Sign In Now!</Link>
+          </div>
+        </Form>
+      </div>
+    </div>
+  )
+}
 
 export default Register
